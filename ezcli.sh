d8a33721cd6e60fca6ee1ee5a534f8264ddf1449
CCIEliu <ccieliu@gmail.com>
2019-02-12 15:14:30 +0800

    Update ezcli.sh
    Add "ezcli" display all NIC name and address

--- ezcli.sh
+++ ezcli.sh
@@ -6,6 +6,13 @@ then
 	echo ""
 	echo "Usage: ezcli [ensXX] ip addr [ADDRESS] [MASK] gw [GATEWAY]"
 	echo ""
+	echo "========================== "
+	ifconfig | grep mtu | awk '{ print $1 }' | grep -v lo >/tmp/nic.tmp
+	/sbin/ip add show | grep -w inet | grep -v '127.0.0.1' | awk '{ print $2 }' >/tmp/ip.tmp
+	paste /tmp/nic.tmp /tmp/ip.tmp
+	echo "========================== "
+	rm -rf /tmp/nic.tmp /tmp/ip.tmp
+	echo ""
 	exit 1
 fi
 
@@ -34,4 +41,4 @@ ifup $editNIC > /dev/null 2>&1
 echo ""
 echo "[IP: $newIP] with [Prefix: $newPrefix] [Gateway: $newGW ]on [NIC: $editNIC] now. Have fun!!"
 echo "Your config backup at ${backupFile}"
-echo ""  
+echo ""
